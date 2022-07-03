1cda92564636276cb09f5325badf787b95b9ef54
Firdaws Lamidi <lamidifirdaws@gmail.com>
2022-07-03 23:14:35 +0100

    first commit

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -3,8 +3,8 @@ import React from 'react'
 const Header = () => {
   return (
     <div className="flex flex-row justify-between bg-purple-600 h-[65px] px-[15px] py-[20px]">
-        <div>
-            <h2 className='fill-white text-[20px] font-[700]'>Meme Generator</h2>
+        <div className='flex items-center'>
+            <h2 className='fill-white text-[20px] font-[700] '>Meme Generator</h2>
         </div>
         <div>
 <p className="fill-white text-[12px] font-[500]">React Course - Project 3</p>
--- src/components/Meme.jsx
+++ src/components/Meme.jsx
@@ -1,17 +1,29 @@
-import React from 'react'
-
+import React from 'react';
+import MemesData from '../memesData';
 const Meme = () => {
+  const [memeImage, setMemeImage] = React.useState("");
+let url;
+const handleClick = () => {
+const memesArray = MemesData.data.memes;
+const randomNumber = Math.floor(Math.random() * memesArray.length);
+setMemeImage((memesArray[randomNumber].url));
+ 
+console.log(url);
+};
   return (
     <div>
-        <main className='m-[30px]'>
-            <form>
-                <div className='flex flex-row'>
-                    <input type = "text" placeholder='Top Text' className='block w-[100%] border-[1px] border-black p-[10px] rounded-[5px] m-2'/>
-                <input type = "text" placeholder='Bottom Text' className='block w-[100%] border-[1px] border-black p-[10px] rounded-[5px] m-2'/>
-                </div>
-                <button className='block w-[100%] bg-purple-500 my-[30px] mx-[auto] p-[10px] rounded-[5px] cursor-pointer'>Get a new meme image</button>
-            </form>
-        </main>
+      <main className='m-[30px]'>
+        <div className='flex flex-col justify-center items-center'>
+          <div className='flex flex-row w-[100%]'>
+            <input type="text" placeholder='Top Text' className='block w-[100%] border-[1px] border-black p-[10px] rounded-[5px] m-2' />
+            <input type="text" placeholder='Bottom Text' className='block w-[100%] border-[1px] border-black p-[10px] rounded-[5px] m-2' />
+          </div>
+         <div className='w-[100%] flex justify-center '> <button onClick={handleClick} className='block w-[100%] bg-purple-500 my-[30px] p-[10px] rounded-[5px] cursor-pointer'>Get a new meme image</button></div>
+        </div>
+      </main>
+      <div className='flex justify-center items-center'>
+        <img src={memeImage} alt="Meme" className='my-[10px] max-w-[100%]' />
+        </div>
     </div>
   )
 }
