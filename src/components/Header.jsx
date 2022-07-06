bd2e54d1d17aae4fb4a9130c761abf9782612e7e
Firdaws Lamidi <lamidifirdaws@gmail.com>
2022-07-06 20:39:53 +0100

    third commit

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -4,10 +4,10 @@ const Header = () => {
   return (
     <div className="flex flex-row justify-between bg-purple-600 h-[65px] px-[15px] py-[20px]">
         <div className='flex items-center'>
-            <h2 className='fill-white text-[20px] font-[700] '>Meme Generator</h2>
+            <h2 className='text-white text-[20px] font-[700] '>Meme Generator</h2>
         </div>
         <div>
-<p className="fill-white text-[12px] font-[500]">React Course - Project 3</p>
+<p className="text-white text-[12px] font-[500]">React Course - Project 3</p>
 
         </div>
     </div>
--- src/components/Meme.jsx
+++ src/components/Meme.jsx
@@ -19,19 +19,28 @@ setMeme(prevMeme => ({
  
 console.log(url);
 };
+
+function handleChange(event) {
+  const {name, value} = event.target;
+  setMeme(prevMeme => ({ ...prevMeme,
+  [name]: value})
+     );
+}
   return (
     <div>
       <main className='m-[30px]'>
         <div className='flex flex-col justify-center items-center'>
           <div className='flex flex-row w-[100%]'>
-            <input type="text" placeholder='Top Text' className='block w-[100%] border-[1px] border-black p-[10px] rounded-[5px] m-2' />
-            <input type="text" placeholder='Bottom Text' className='block w-[100%] border-[1px] border-black p-[10px] rounded-[5px] m-2' />
+            <input type="text" name='topText' value={meme.topText} onChange={handleChange} placeholder='Top Text' className='block w-[100%] border-[1px] border-black p-[10px] rounded-[5px] m-2' />
+            <input type="text" name="bottomText" value={meme.bottomText} onChange={handleChange} placeholder='Bottom Text' className='block w-[100%] border-[1px] border-black p-[10px] rounded-[5px] m-2' />
           </div>
-         <div className='w-[100%] flex justify-center '> <button onClick={handleClick} className='block w-[100%] bg-purple-500 my-[30px] p-[10px] rounded-[5px] cursor-pointer'>Get a new meme image</button></div>
+         <div className='w-[100%] flex justify-center '> <button onClick={handleClick} className='block w-[100%] bg-purple-500 my-[30px] p-[10px] rounded-[5px] cursor-pointer text-white'>Get a new meme image</button></div>
         </div>
       </main>
-      <div className='flex justify-center items-center'>
-        <img src={meme.randomImage} alt="Meme" className='my-[10px] max-w-[100%]' />
+      <div className='flex justify-center items-center relative'>
+        <img src={meme.randomImage} alt="Meme" className='my-[10px] max-w-[100%] rounded-[3px]' />
+        <h2 className="absolute w-[80%] text-center left-[50%] translate-x-[-50%] text-[2em] py-3 mx-0 my-[15px]top-0 text-white translate-y-[-110px] uppercase">{meme.topText}</h2>
+                <h2 className="absolute w-[80%] text-center left-[50%] translate-x-[-50%] text-[2em] py-3 mx-0 my-[15px] bottom-0 text-white uppercase">{meme.bottomText}</h2>
         </div>
     </div>
   )
